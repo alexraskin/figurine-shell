0899c65ebd742d3ed20076858aa8cb1b80487eb8
alexraskin <alexraskin@fastmail.fm>
2024-04-03 17:02:22 -0700

    add license - update script

--- install.sh
+++ install.sh
@@ -7,16 +7,10 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-if [ $# -lt 1 ]; then
-  echo "Usage: $0 '<figurine_text>'" >&2
-  exit 1
-fi
-
-FIGURINE_TEXT="$1"
-
 
 URL="https://github.com/arsham/figurine/releases/download/v1.3.0/figurine_linux_amd64_v1.3.0.tar.gz"
 FIGURINE_SCRIPT="/etc/profile.d/figurine.sh"
+HOSTNAME=$(hostname)
 
 if ! command -v wget &> /dev/null || ! command -v tar &> /dev/null; then
     echo "This script requires 'wget' and 'tar' to run." >&2
@@ -41,7 +35,7 @@ cat > "$FIGURINE_SCRIPT" <<EOF
 #!/bin/bash
 # Figurine greeting script
 echo ""
-/usr/local/bin/figurine -f "3d.flf" "$FIGURINE_TEXT"
+/usr/local/bin/figurine -f "3d.flf" "$HOSTNAME"
 echo ""
 EOF
 
